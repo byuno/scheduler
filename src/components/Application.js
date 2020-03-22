05ca25865c624fce41e39d492eae8e283f4a4a40
Bobby Uno <robert.y.uno@gmail.com>
2020-03-22 09:09:45 -0700

    finished getInterview

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment"
 import React, { useState, useEffect } from "react";
-import { getAppointmentsForDay } from "helpers/selectors.js"
+import { getAppointmentsForDay, getInterview } from "helpers/selectors.js"
 
 const axios = require('axios')
 
@@ -11,7 +11,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
   
   const setDay = day => setState({ ...state, day });
@@ -22,10 +23,34 @@ useEffect(() => {
   Promise.all([
     Promise.resolve(axios.get("/api/days")),
     Promise.resolve(axios.get("/api/appointments")),
-  ]).then((all) => { setState(prev => ({ days: all[0].data, appointments: all[1].data })) })
+    Promise.resolve(axios.get("/api/interviewers")),
+  ]).then((all) => { 
+    setState(prev => ({ 
+      days: all[0].data, 
+      appointments: all[1].data, 
+      interviewers: all[2].data})) 
+      console.log('this is what all[2].data looks like', all[2].data)
+    })
 }, [])
 
 const appointments = getAppointmentsForDay(state, state.day);
+
+console.log("this is appointments", appointments);
+
+
+
+const schedule = appointments.map((appointment) => {
+const interview = getInterview(state, appointment.interview);
+
+  return (
+    <Appointment
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
+    />
+  );
+});
 
   return (
     <main className="layout">
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,6 @@
 export function getAppointmentsForDay(state, day) {
- 
+ //console.log('this is state', state);
+ //console.log('this is day', day)
   //wconsole.log('this is what stat.days looks like', state.days[0].name)
   
   let daysArray = [];
@@ -19,4 +20,20 @@ export function getAppointmentsForDay(state, day) {
   }
   
   return appointmentsArray;
+};
+
+
+export function getInterview(state, interview) {
+  
+  if (!interview) {
+    return null
+  }
+
+  let interviewObj = {};
+  const interviewerID = interview['interviewer'].toString();
+
+  interviewObj = {  "student": interview.student, 
+                    "interviewer": state.interviewers[interviewerID] }
+
+  return interviewObj;
 };
