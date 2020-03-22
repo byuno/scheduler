b81cff90194804d5f21084ce8711c32a5a8ccc10
Bobby Uno <robert.y.uno@gmail.com>
2020-03-21 21:24:10 -0700

    clean up code

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,92 +7,9 @@ import { getAppointmentsForDay } from "helpers/selectors.js"
 const axios = require('axios')
 
 
-// /Users/bobby/lighthouse/scheduler/src/helpers/selectors.js
-
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
-
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "9pm",
-//     interview: {
-//       student: "Bobby",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "10pm",
-//     interview: {
-//       student: "Bob",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },  
-//   {
-//     id: 5,
-//     time: "11pm",
-//     interview: {
-//       student: "Robert",
-//       interviewer: {
-//         id: 5,
-//         name: "Sven Jones",
-//         avatar: "https://i.imgur.com/twYrpay.jpg",
-//       }
-//     }
-//   }
-// ];
-
-
-
-
-
 export default function Application(props) {
 
-
-  
-  
-  const [state, setState] = useState({
+const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {}
