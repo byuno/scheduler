8248e761b17d2bedf26eb98248b8d8c92ddcdf5f
Bobby Uno <robert.y.uno@gmail.com>
2020-03-29 15:41:38 -0700

    completed cypress tests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,15 @@
 describe("Navigation", () => {
-  it("should visit root", () => {
+  it("1. should visit root", () => {
     cy.visit("/");
   });
+
+  it("2. should navigate to Tuesday", () => {
+    cy.visit("/");
+    
+    cy.contains("[data-testid=day]", "Tuesday")
+    .click()
+    .should("have.class", "day-list__item day-list__item--selected");
+  });
+
+
 });
